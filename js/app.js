28b0ee36badcfba01c12f06785c56c92abc1c0d5
Shaun van Ardenne <s.vanardenne@gmail.com>
2020-07-23 21:44:55 -0700

    set up start of pjoject

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const qwerty = document.getElementById('qwerty');
+console.log(qwerty);
+const phrase = document.getElementById('phrase');
+console.log(phrase);
+let missed = 0;
+console.log(missed);
+const mainContainer = document.querySelector('.main-container');
+console.log(mainContainer);
+
+mainContainer.addEventListener('click', (e) => {
+    
+});
