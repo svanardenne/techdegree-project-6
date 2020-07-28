a496e517282e6fb9ec0acc0ae0f5e9105e232b2b
Shaun van Ardenne <s.vanardenne@gmail.com>
2020-07-27 17:22:30 -0700

    Finished base project.

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ let missed = 0;
 const mainContainer = document.querySelector('.main-container');
 const overlay = document.getElementById('overlay');
 const ul = document.querySelector('ul');
+const ol = document.querySelector('ol');
+const tries = document.querySelector('.tries');
 
 ////////////
 // Arrays
@@ -55,15 +57,30 @@ function addPhraseToDisplay(arr) {
 // Checks to see if letter chosen is a letter in the phrase and either reveals 
 // the letter or subtracts health
 function checkLetter(key) {
-    const letter = document.getElementsByClassName("letter");
-    let check = null;
-    for (let i = 0; i < letter.length; i++) {
-        if (key.textContent.toLowerCase() === letter[i].textContent.toLowerCase()) {
-            letter[i].classList.add('show');
-            check = key.textContent;
+    const checkLetter = document.getElementsByClassName("letter");
+    let match = null;
+    for (let i = 0; i < checkLetter.length; i++) {
+        if (key.textContent.toLowerCase() === checkLetter[i].textContent.toLowerCase()) {
+            checkLetter[i].classList.add('show');
+            match = key.textContent;
         }
     }
-    return check;
+    return match;
+}
+
+function checkWin() {
+    const show = document.querySelectorAll('.show');
+    const letter = document.querySelectorAll('.letter');
+    console.log(overlay);
+    if (show.length === letter.length) {
+        overlay.className = 'win';
+        overlay.style.display = 'flex';
+        overlay.innerHTML = `<h1>You Win!</h1><h3>Hit reset to play again</h3>`;
+    } else if (missed >= 5) {
+        overlay.className = 'lose';
+        overlay.style.display = 'flex';
+        overlay.innerHTML = `<h1>You lost</h1><h3>Hit reset to play again</h3>`;
+    }
 }
 
 /////////////
@@ -71,7 +88,7 @@ function checkLetter(key) {
 /////////////
 
 //Starts the game and removes the overlay
-mainContainer.addEventListener('click', (e) => {
+overlay.addEventListener('click', (e) => {
     overlay.style.display = 'none';
 });
 
@@ -81,12 +98,18 @@ addPhraseToDisplay(phraseArray);
 
 // Listens for keypresses and passes them to checkLetter()
 qwerty.addEventListener('click', (event) => {
-    const scoreboard = document.getElementById('scoreboard');
     let keyPress = event.target;
     if (keyPress.tagName === 'BUTTON') {
     keyPress.className = 'chosen';
     keyPress.setAttribute('disabled', 'disabled');
-    const checked = checkLetter(keyPress);
-    console.log(checked);
+    const letterFound = checkLetter(keyPress);
+    console.log(letterFound);
+        if (letterFound === null) {
+            missed += 1;
+            let li = ol.firstElementChild;
+            ol.removeChild(li);
+            
+        }
     }
+    checkWin()
 });
